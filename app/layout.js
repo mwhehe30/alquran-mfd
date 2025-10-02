2db4300687d9d1f7c81eab3b8956db1cd082b824
zaky <zakymubarok055022009@gmail.com>
2025-10-02 22:03:44 +0700

    hahhhhh

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,29 @@
-import MobileContainer from "@/components/mobile-container";
-import { Inter, Scheherazade_New } from "next/font/google";
-import "./globals.css";
+import MobileContainer from '@/components/mobile-container';
+import { Inter, Scheherazade_New } from 'next/font/google';
+import './globals.css';
 
 const arabic = Scheherazade_New({
-  variable: "--font-arabic",
-  weight: ["400", "500", "600", "700"],
-  subsets: ["arabic"],
+  variable: '--font-arabic',
+  weight: ['400', '500', '600', '700'],
+  subsets: ['arabic'],
 });
 
 const latin = Inter({
-  variable: "--font-latin",
-  subsets: ["latin"],
+  variable: '--font-latin',
+  subsets: ['latin'],
 });
 
 export const metadata = {
   title: {
-    template: "%s | Alquran",
-    default: "Alquran",
+    template: '%s | Alquran',
+    default: 'Alquran',
   },
-  description: "Alquran",
+  description: 'Alquran',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${arabic.variable} ${latin.variable} antialiased bg-gradient-to-br from-green-50 to-blue-50`}
       >
